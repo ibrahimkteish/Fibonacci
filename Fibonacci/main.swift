610f832e096b904e865f4fc4fa957b77b4acd97a
Ibrahim Kteish <infinitybob@gmail.com>
2016-10-09 15:37:01 +0300

    Initial Commit

---
+++ Fibonacci/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Fibonacci
+//
+//  Created by Ibrahim Kteish on 10/9/16.
+//  Copyright © 2016 Ibrahim Kteish. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
